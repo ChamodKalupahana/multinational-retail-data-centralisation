1dde9940d6ace4b9bd5f5081200537ab069d0ca0
Chamod Kalupahana <chamodkalupahana@gmail.com>
2024-01-30 15:04:41 +0000

    Successfully connected to database and listed tables

--- database_utils.py
+++ database_utils.py
@@ -1,5 +1,7 @@
 # Import modules
 import yaml
+from sqlalchemy import create_engine, MetaData
+import pandas as pd
 
 # Define class
 class DatabaseConnector:
@@ -14,9 +16,65 @@ class DatabaseConnector:
                 print(exc)
         
         return creds
+    
+    def init_db_engine_v1(self):
+        creds = self.read_db_creds()
+
+        DATABASE_TYPE = 'postgresql'
+        DBAPI = 'psycopg2'
+        HOST = creds['RDS_HOST']
+        USER = creds['RDS_USER']
+        PASSWORD = creds['RDS_PASSWORD']
+        DATABASE = creds['RDS_DATABASE']
+        PORT = creds['RDS_PORT']
+
+        engine = create_engine(f"{DATABASE_TYPE}+{DBAPI}://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}")
+
+        engine = create_engine(f'postgresql+psycopg2://{USER}:{PASSWORD}\@{HOST}/{DATABASE}')
+
+        engine = create_engine(f'postgresql://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}')
+
+        engine = create_engine(f"postgresql+pg8000://user:pa$$w0rd@12.34.56.789/mydatabase?charset=utf8mb4")
+
+        engine.execution_options(isolation_level='AUTOCOMMIT').connect()
+
+        return engine
+
+    def init_db_engine(self):
+        credentials = self.read_db_creds()
+        engine_connection = f"postgresql+psycopg2://{credentials['RDS_USER']}:{credentials['RDS_PASSWORD']}@{credentials['RDS_HOST']}:{credentials['RDS_PORT']}/{credentials['RDS_DATABASE']}"
+        engine = create_engine(engine_connection)
+
+        return engine
+    
+    def list_db_tables(self):
+        engine = self.init_db_engine()
+        metadata = MetaData()
+        metadata.reflect(bind=engine)
+    
+        # Extract table names
+        table_names = pd.DataFrame(metadata.tables.keys())
+
+        return table_names
 
 # Test class
-
 test = DatabaseConnector()
-creds = test.read_db_creds()
-print(creds)
+table_names = test.list_db_tables()
+
+print(table_names)
+
+# For testing engine connection to RDS dataset
+""" 
+with engine.execution_options(isolation_level='AUTOCOMMIT').connect() as conn:
+    # Reflect the metadata of the database tables
+    metadata = MetaData()
+    metadata.reflect(bind=engine)
+    
+    # Extract table names
+    table_names = metadata.tables.keys()
+    
+    #return table_names
+
+print('Help')
+
+"""
