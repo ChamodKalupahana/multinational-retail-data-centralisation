7708851583c287481653931e62d6421aa1d3f1fa
Chamod Kalupahana <chamodkalupahana@gmail.com>
2024-01-30 12:00:32 +0000

    Created read_bd_creds method

--- database_utils.py
+++ database_utils.py
@@ -1,3 +1,22 @@
+# Import modules
+import yaml
+
+# Define class
 class DatabaseConnector:
     def __init__(self) -> None:
         pass
+
+    def read_db_creds(self):
+        with open("db_creds.yaml", "r") as stream:
+            try:
+                creds = yaml.safe_load(stream)
+            except yaml.YAMLError as exc:
+                print(exc)
+        
+        return creds
+
+# Test class
+
+test = DatabaseConnector()
+creds = test.read_db_creds()
+print(creds)
