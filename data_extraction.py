ca857c472c46e974f0e9a0f00a78305457df2b79
Chamod Kalupahana <chamodkalupahana@gmail.com>
2024-01-31 19:16:00 +0000

    Created card pdf reader

--- data_extraction.py
+++ data_extraction.py
@@ -1,5 +1,6 @@
 # Import modules
 import pandas as pd
+import tabula as ta
 
 from database_utils import DatabaseConnector
 
@@ -14,10 +15,22 @@ class DataExtractor:
             table = pd.read_sql_table(table_name, engine)
         return table
 
+    def retrieve_pdf_data(self):
+        pdf_path = 'card_details.pdf'
+
+        card_details = ta.read_pdf(pdf_path, pages='all')
+
+        return pd.DataFrame(card_details)
+
+
 # only run if this file is run
 if __name__ == "__main__":
     test = DataExtractor()
     DatabaseConnector_instance = DatabaseConnector()
 
-    table = test.read_rds_table(DatabaseConnector_instance, table_name = 'legacy_store_details')
-    print(table)
+    # table = test.read_rds_table(DatabaseConnector_instance, table_name = 'legacy_store_details')
+    # print(table)
+
+    pdf = test.retrieve_pdf_data()
+    print(pdf)
+
